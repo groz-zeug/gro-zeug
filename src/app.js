66aa2451f6f3aa90d67c6dfa10c009f0ec399172
groz-zeug <117457332+groz-zeug@users.noreply.github.com>
2022-11-05 13:34:57 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const ideaDisplay = document.querySelector("#ideas");
+
+fetch(baseURL + "/ideas")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((idea) => {
+      const title =
+        `
+      <div id='idea'>
+            <h5><a href="` +
+        idea.url +
+        `" target="_blank">` +
+        idea.title +
+        `</a></h5>
+        </div>
+        
+        <br>`;
+      ideaDisplay.insertAdjacentHTML("beforeend", title);
+    });
+  });
